f89149f98b4662d94e8e79d579920ddcc5a4976c
Dalius Ram <ramonas.dalius@gmail.com>
2022-04-04 21:09:09 +0300

    Funkcijos done

--- 04-03.js
+++ 04-03.js
@@ -136,7 +136,7 @@ skaitmenuKiekisSkaiciuje(NaN);
 
 // 4
 console.log(`-----------------------`)
-let sarasas = [];
+let sarasas = ['pomidoras'];
 function didziausiasSkaiciusSarase(arr) {
     let didziausias = arr[0];
     if (Array.isArray(arr) === false) {
@@ -145,10 +145,87 @@ function didziausiasSkaiciusSarase(arr) {
         return 'Pateiktas sarasas negali buti tuscias';
     } else {
         for (let i = 0; i <= arr.length - 1; i++) {
-            if (didziausias < arr[i]) {
+           if (didziausias < arr[i]) {
                 didziausias = arr[i];
             }
         }
     }  return didziausias;
 }
-console.log(didziausiasSkaiciusSarase(sarasas));
+console.log( didziausiasSkaiciusSarase( [ 1 ] ) );
+console.log( didziausiasSkaiciusSarase( [ 1, 2, 3 ] ) );
+console.log( didziausiasSkaiciusSarase( [ -5, 78, 14, 0, 18 ] ) );
+console.log( didziausiasSkaiciusSarase( [ 69, 69, 69, 69, 66 ] ) );
+console.log( didziausiasSkaiciusSarase( [ -1, -2, -3, -4, -5, -6, -7, -8 ] ) );
+console.log( didziausiasSkaiciusSarase( 'pomidoras' ) );
+console.log( didziausiasSkaiciusSarase( [] ) );
+
+
+// 5
+console.log(`-----------------------`)
+function isrinktiRaides(text, letterNumber) {
+    let surinktosRaides = '';
+    if (typeof text !== 'string') {
+        return 'Pirmasis kintamasis yra netinkamo tipo.';
+    } else if (text.length === 0 || text.length > 100){
+        return 'Pirmojo kintamojo reikšmė yra netinkamo dydžio.';
+    }
+    if (typeof letterNumber !== 'number') {
+        return 'Antrasis kintamasis yra netinkamo tipo.';
+    } else if (letterNumber <= 0) {
+        return 'Antrasis kintamasis turi būti didesnis už nulį.';
+    }
+    if ( letterNumber > text.length) {
+        return 'Antrasis kintamasis turi būti ne didesnis už pateikto teksto ilgį.';
+    }
+    for (let i = letterNumber - 1; i <= text.length - 1; i+=letterNumber) {
+        surinktosRaides = surinktosRaides + text[i];
+    }
+    return surinktosRaides;
+};
+console.log(isrinktiRaides('Pirmojo kintamojo reikšmė yra netinkamo dydžio.Pirmojo kintamojo reikšmė yra netinkamo dydžio.Pirmojo kintamojo reikšmė yra netinkamo dydžio.', 5));
+console.log(isrinktiRaides('', 5));
+console.log(isrinktiRaides(false, 5));
+console.log(isrinktiRaides('hello', '5'));
+console.log(isrinktiRaides('hello', -1));
+console.log(isrinktiRaides('hello', 10));
+
+console.log(isrinktiRaides('hello', 2));
+console.log(isrinktiRaides('abcdefg', 2));
+console.log(isrinktiRaides('abcdefghijkl', 3 ) );
+console.log(isrinktiRaides('abc', 0 ) );
+console.log(isrinktiRaides('abc', 4 ) );
+console.log(isrinktiRaides(1561, 2 ) );
+
+
+//  6
+
+function dalyba(a, b) {
+    const dalyba = a / b;
+    if (typeof a !== 'number' || typeof b !== 'number') {
+        return 'Bent vienas is kintamuju nera skaicius';
+    }
+    if (isNaN(a) || isNaN(b)) {
+        return 'Kintamieji turi buti tikri skaiciai';
+    }
+    if (b === 0) {
+        return 'Dalyba is 0 nera galima';
+    }
+    if (a === Infinity || b === Infinity) {
+        return 'Kintamieji turi buti tikri skaiciai'
+    }
+    return dalyba;
+};
+
+console.log(dalyba('a', 5));
+console.log(dalyba(5, 'b'));
+console.log(dalyba(5, NaN));
+console.log(dalyba(NaN, 5));
+console.log(dalyba([5, 7], 5));
+console.log(dalyba(4, 0));
+console.log(dalyba(0, 4));
+console.log(dalyba(Infinity, 4));
+console.log(dalyba(12, 4));
+console.log(dalyba(0, 3));
+console.log(dalyba(-2, 300));
+
+
