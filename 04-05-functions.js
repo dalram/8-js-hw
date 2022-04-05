6615ebe525e1ab8f8accafdbe836af26d638d5d6
Dalius Ram <ramonas.dalius@gmail.com>
2022-04-05 17:13:57 +0300

    funkcijos done

--- 04-05-functions.js
+++ 04-05-functions.js
@@ -16,7 +16,7 @@ function palyginimas(a, b) {
         return `Ivestas pirmasis skaicius ${a} didesnis uz ${b}`;
     }
     if (a < b) {
-        return `Ivestas antasis skaicius ${b} didesnis uz pirmaji ${a}`;
+        return `Ivestas antrasis skaicius ${b} didesnis uz pirmaji ${a}`;
     }
     if (a === b) {
         return `Ivesti skaiciai lygus`;
@@ -41,7 +41,7 @@ console.log(keliamiejiMetai(1500));
 // 4
 console.log('--------------');
 function kvadratu(a) {
-    let skaiciausKvadratas
+    let skaiciausKvadratas;
     if (typeof a !== 'number') {
         return 'Error, funckija priima tik skaicius'
     } else {
@@ -54,8 +54,8 @@ console.log(kvadratu());
 console.log('--------------');
 function intervaloSuma (a) {
     let forSuma = 0;
-    if (typeof a !== 'number') {
-        return 'Error, funckija priima tik skaicius'
+    if (typeof a !== 'number' || !isFinite(a)) {
+        return 'Error, funckija priima tik sveikuosius skaicius'
     }
     if (a <= 1) {
         return 'Ivestas skaicius turi buti didesnis uz 1';
@@ -66,12 +66,101 @@ function intervaloSuma (a) {
         return forSuma;
     }
 };
-console.log(intervaloSuma(2));
+console.log(intervaloSuma(15));
 
 // 6
 console.log('--------------');
 function dalinasiIsSveikujuSkaiciu (a) {
-    if (typeof a !== 'number') {
-        return 'Error, funckija priima tik skaicius'
+    if (typeof a !== 'number' || !isFinite(a)) {
+        return 'Error, funckija priima tik sveikuosius skaicius'
+    }
+    let dalikliai = 0;
+    for (let i = 2; i < a; i++) {
+        if (a % i === 0) {
+            dalikliai++; 
+        } else {
+            continue;
+        }
+    }
+    return dalikliai;
+}
+console.log(dalinasiIsSveikujuSkaiciu(30));
+
+// 7
+console.log('--------------');
+function tekstoIlgis(text) {
+    if (typeof text !== 'string') {
+        return 'Error: funckija priima tik teksta';
+    }
+    return `Ivestas tekstas yra: ${text} ---> teksto simboliu kiekis yra ${text.length}.`
+};
+console.log(tekstoIlgis('Begemotas'))
+
+// 8
+console.log('--------------');
+function telefonoNumeris(arr) {
+    if (typeof arr !== 'object') {
+        return 'Error: funkcijai turi buti pateiktas masyvas';
+    }
+    if (arr.length !== 10) {
+        return 'Error: funkcijai turi buti pateiktas 10 skaiciu masyvas';
+    }
+    let telNr = '';
+    for (let i = 0; i < arr.length; i++) {
+        const number = arr[i];
+        if (typeof number !== 'number') {
+            return 'Error: Masyva turi sudaryt desimt skaiciu.'
+        } 
+        if (number >= 10 || number < 0) {
+            return 'Error: Visu masyve nurodytu skaiciu reiksmes turi buti nuo 0 iki 9.'
+        }
+        telNr += number;
+    }
+    return `(${telNr[0] + telNr[1] + telNr[2]}) ${telNr[4] + telNr[5] + telNr[6]}-${telNr[7] + telNr[8] + telNr[9]}`;
+};
+
+console.log(telefonoNumeris([10, 20, 30, 40, 50, 1, 2, 3, 4]));
+console.log(telefonoNumeris([10, 20, 30, 40, 50, 1, 2, 3, 4, 5, 5]));
+console.log(telefonoNumeris(["10", 20, 30, 40, 50, 1, '2', 3, 4, 5]));
+console.log('--------------');
+console.log(telefonoNumeris([9, 2, 3, 4, 5, 1, 2, 3, 4, 5]));
+
+// 9
+console.log('--------------');
+function ilgesnisKintamasis(texta, textb) {
+    if (typeof texta !== 'string' || typeof textb !== 'string') {
+        return 'Error: Abu kintamieji turi buti tekstinio tipo'
+    }
+    if (texta.length > textb.length) {
+        return 'Pirmasis pateiktas tekstas yra ilgesnis.'
+    } else if (texta.length < textb.length) {
+        return 'Antrasis pateiktas tekstas yra ilgesnis.'
+    } else {
+        return 'Tekstu ilgiai yra vienodi.'
     }
 }
+console.log(ilgesnisKintamasis('v', 'v'));
+console.log(ilgesnisKintamasis("5", 7));
+console.log(ilgesnisKintamasis(5, "5"));
+console.log(ilgesnisKintamasis('5','tru'));
+
+// 10
+console.log('--------------');
+function sumaA(text) {
+    if (typeof text !== 'string') {
+        return 'Error: Pateiktas kintamasis turi buti tekstinio tipo.'
+    }
+    let aCount = 0;
+    for (let i=0; i < text.length; i++) {
+        const raide = text[i];
+        if (raide === 'a') {
+            aCount++;
+        } else {
+            continue;
+        }
+    }
+    return aCount;
+}
+console.log(sumaA(5));
+console.log(sumaA(false));
+console.log(sumaA('faaaaaaaaaaaaaaaaaaaalse'));
